034bdce79fa2b2f8241e4387330f498dfef5364b
ilnur13 <tukaevvv_1994@icloud.com>
2023-06-27 16:23:56 +0600

    1

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <v-container>
       <HeaderVue></HeaderVue>
       <NavigationVue></NavigationVue>
+          <LeftblockVue></LeftblockVue>
+          <RightblockVue></RightblockVue>
+          <SliderVue></SliderVue>
+      <FooterVue></FooterVue>
       <v-main>
         <router-view/>
       </v-main>
@@ -13,12 +17,20 @@
 <script>
   import HeaderVue from './components/common/HeaderVue.vue';
   import NavigationVue from './components/common/NavigationVue.vue';
-
+  import LeftblockVue from './components/common/LeftblockVue.vue';
+  import RightblockVue from './components/common/RightblockVue.vue';
+  import SliderVue from './components/common/SliderVue.vue';
+  import FooterVue from './components/common/FooterVue.vue';
+  
 export default {
   name: 'App',
   components: {
     HeaderVue,
-    NavigationVue
+    NavigationVue,
+    LeftblockVue,
+    RightblockVue,
+    SliderVue,
+    FooterVue
   },
   data: () => ({
     //
---
+++ src/components/common/SliderVue.vue
@@ -0,0 +1,76 @@
+<template>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows="hover"
+    >
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+      >
+        <v-sheet
+          :color="colors[i]"
+          height="100%"
+          class="d-flex"
+        >
+        <v-img
+                    :src="slide.image"
+                    height="100%"
+                    width="50%"
+                    object-fit="cover"
+                    z-index="0"
+        ></v-img>
+          <!-- <div class="d-flex fill-height justify-end align-center" style="margin-right: 90px; width: 50%;" > -->
+            <div class="text-h4" style="width: 50%;">
+                
+              <h3 class="slider__title">{{ slide.title }} Slide</h3>
+              <p class="text-body-2">{{ slide.text }}</p>
+              <span class="slider__price">{{ slide.price }}</span>
+              <v-row>
+                <v-btn v-for="(button, index) in slide.buttons" :key="index" :color="button.color" class="white--text" style="gap: 10px;">{{ button.text }}</v-btn>
+              </v-row>
+            </div>
+          <!-- </div> -->
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        colors: ['grey', 'grey', 'grey', 'grey', 'grey'],
+        slides: [
+          {
+            image: '../src/assets/header/img/slider_1.jpg',
+            title: 'Атлет ГЗЧ-2500 генератор звуковой частоты',
+            text: 'Текст первой страницы',
+            price: '170',
+            buttons: [
+              { text: 'Кнопка 1', color: '#e10018' },
+              { text: 'Кнопка 2', color: '#e10018' },
+            ],
+          },
+          {
+            image: '../../assets/header/img/Slider_1.jpg',
+            title: 'Bhdkv',
+            text: 'Текст второй страницы',
+            buttons: [
+              { text: 'Кнопка 1', color: '#e10018' },
+              { text: 'Кнопка 2', color: '#e10018' },
+            ],
+          },
+          // Добавьте остальные слайды с разным текстом и кнопками
+        ],
+      };
+    },
+  };
+  </script>
+  
+  
+
+<Style lang="scss">
+
+</Style>
